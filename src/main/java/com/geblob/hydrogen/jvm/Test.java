c3246cc321f815cca1face67babcb8cf113bc493
zhudahai <nextleave@gmail.com>
2020-04-20 17:24:04 +0800

    test methodHandle.

---
+++ src/main/java/com/geblob/hydrogen/jvm/Test.java
@@ -0,0 +1,35 @@
+package com.geblob.hydrogen.jvm;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+
+import static java.lang.invoke.MethodHandles.lookup;
+
+public class Test {
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        void thinking() {
+            try {
+                MethodType mt = MethodType.methodType(void.class);
+                MethodHandle mh = lookup().findVirtual(GrandFather.class, "thinking", mt).bindTo(new Test().new GrandFather());
+                mh.invokeExact();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        (new Test().new Son()).thinking();
+    }
+}
